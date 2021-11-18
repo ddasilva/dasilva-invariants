07c331c70cda1dcc3beca23c6085b91bd98c168e
Daniel da Silva <daniel.e.dasilva@nasa.gov>
2021-11-17 19:07:08 -0500

    Completed test of dipole model on LFM grid

---
+++ dasilva_invariants/meshes.py
@@ -0,0 +1,100 @@
+"""Tools for obtaining meshes for use in calculating invariants.
+
+Meshes are grids + magnetic field vectors at those grid points. They
+are instances of pyvista.StructuredGrid. PyVista is used throughout
+this project.
+
+In this module, all grids returned are in units of Re and all magnetic
+fields are in units of Gauss.
+"""
+
+from astropy import constants, units
+import numpy as np
+from pyhdf.SD import SD, SDC
+import pyvista
+
+
+def _fix_lfm_hdf4_array_order(data):
+    """Apply fix to LFM data to account for strange format in HDF4 file.
+
+    This must be called on all data read from an LFM HDF4 file.
+
+    Adapted from pyLTR, which has the following comment:
+       Data is stored in C-order, but shape is transposed in
+       Fortran order!  Why?  Because LFM stores its arrays using
+       the rather odd A++/P++ library.  Let's reverse this evil:
+    Args
+      data: array with 3 dimensions
+    Returns
+      data: array with (i, j, k) fixed
+    """
+    s = data.shape
+    data = np.reshape( data.ravel(), (s[2], s[1], s[0]), order='F' )
+    return data
+
+
+def _convert_to_pyvista_order(data):
+    """This function converts an array used in a PyVista mesh from its 3D
+    form to its a 1D form required to be stored in the mesh.
+
+    Args
+      data: array with three dimensions
+    Returns
+      data: 1D flattened array suitable to be added to a PyVista mesh
+    """
+    s = data.shape
+    data = np.reshape(data.ravel(), (s[2], s[1], s[0]), order='F').flatten()
+    return data
+
+
+def get_dipole_mesh_on_lfm_grid(lfm_hdf4_path):
+    """Get a dipole field on a LFM grid. Uses an LFM HDF4 file to obtain
+    the grid.
+
+    Args
+      lfm_hdf4_path: Path to LFM hdf4 file
+    Returns
+      mesh: pyvista.StrucutredGrid instance, mesh on LFM4 grid with dipolate
+      field values.
+    """
+    # Read LFM grid from HDF file and attach units of cm
+    # ------------------------------------------------------------------------
+    hdf = SD(lfm_hdf4_path, SDC.READ)
+    X_grid = _fix_lfm_hdf4_array_order(hdf.select('X_grid').get())
+    Y_grid = _fix_lfm_hdf4_array_order(hdf.select('Y_grid').get())
+    Z_grid = _fix_lfm_hdf4_array_order(hdf.select('Z_grid').get())
+
+    X_grid *= units.cm
+    Y_grid *= units.cm
+    Z_grid *= units.cm
+
+    X_grid = X_grid.to(units.km)   # prevent overflow
+    Y_grid = Y_grid.to(units.km)
+    Z_grid = Z_grid.to(units.km)
+    
+    # Calculate dipole model
+    # ------------------------------------------------------------------------
+    # Dipole model, per Kivelson and Russel equations
+    # 6.3(a)-(c), page 165.
+    Mz = -8e15 * units.T * units.m**3 
+    r = np.sqrt(X_grid**2 + Y_grid**2 + Z_grid**2)
+    Bx = 3 * X_grid * Z_grid * Mz / r**5
+    By = 3 * Y_grid * Z_grid * Mz / r**5
+    Bz = (3 * Z_grid**2 - r**2) * Mz / r**5
+
+    # Create PyVista structured grid.
+    # ------------------------------------------------------------------------
+    X_grid_in_re = (X_grid / constants.R_earth).to(1).value
+    Y_grid_in_re = (Y_grid / constants.R_earth).to(1).value
+    Z_grid_in_re = (Z_grid / constants.R_earth).to(1).value
+
+    mesh = pyvista.StructuredGrid(X_grid_in_re, Y_grid_in_re, Z_grid_in_re)
+    
+    B = np.empty((mesh.n_points, 3))
+    B[:, 0] = _convert_to_pyvista_order(Bx.to(units.G).value)
+    B[:, 1] = _convert_to_pyvista_order(By.to(units.G).value)
+    B[:, 2] = _convert_to_pyvista_order(Bz.to(units.G).value)
+    mesh['B'] = B
+
+    # Return output
+    return mesh
