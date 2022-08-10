d597d9073604e104a3bc2aa6f13e4f955636c865
Daniel da Silva <mail@danieldasilva.org>
2022-08-10 07:30:20 -0600

    Cleanup notebooks into other repo

--- setup.sh
+++ setup.sh
@@ -1,7 +1,10 @@
+# Sets environemntal variables for geopack.
+# See also: bin/update_geopack.py
 
-# Sets environemntal variables for geopack. See also:
-# bin/update_geopack.py
+## EDIT HERE: this is the base for all stored geopack data. Change to something
+# suitable for your system.
 export gp_base=/glade/u/home/danieldas/scratch/geopack/
+## END EDIT HERE
 
 export KPDATA_PATH=$gp_base/kp
 export OMNIDATA_PATH=$gp_base/omni
